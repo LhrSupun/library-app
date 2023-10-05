848b8eb0b4dd4fa5da63b90176d7e236b72afd42
Lahiru Supun <lhrsupun@gmail.com>
2023-10-05 10:55:43 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const connectDB = require('./config/database,js');
+const connectDB = require('./config/database');
+const books = require('./routes/api/books');
+const cors = require('cors');
 const app = express()
 let port;
 
@@ -10,10 +12,13 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 connectDB();
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+app.use('/api/books', books);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
